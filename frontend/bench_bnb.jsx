481cfd8c9d97bcef5600a380da58f476e81a2ab5
Scott Heng <scottheng3@gmail.com>
2017-03-13 01:33:46 -0700

    add functions to make ajax requests

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as APIUtil from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
+    window.signup = APIUtil.signup;
+    window.login = APIUtil.login;
+    window.logout = APIUtil.logout;
+    
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
