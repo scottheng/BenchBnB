af9876992aeea5ed169f15deb55854fc9ff30751
Scott Heng <scottheng3@gmail.com>
2017-03-13 02:11:53 -0700

    create store

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as APIUtil from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+    window.store = configureStore();
     window.signup = APIUtil.signup;
     window.login = APIUtil.login;
     window.logout = APIUtil.logout;
-    
+
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
---
+++ frontend/store/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../reducers/root_reducer';
+import thunk from 'redux-thunk';
+
+const configureStore = (preloadedState = {}) => {
+	const store = createStore(
+		rootReducer,
+		preloadedState, 
+		applyMiddleware(thunk));
+	return store;
+};
+
+export default configureStore;
